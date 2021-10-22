84be9228512b9ee5f5a979c4449aaeada559cee4
Leonid <loginov.l93@gmail.ocm>
2021-10-22 05:06:34 +0300

    Improve ControllerExceptionHandler

--- src/main/java/com/example/shorturl/exeption/ControllerExceptionHandler.java
+++ src/main/java/com/example/shorturl/exeption/ControllerExceptionHandler.java
@@ -3,12 +3,14 @@ package com.example.shorturl.exeption;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.context.request.WebRequest;
 
 import java.time.ZonedDateTime;
+import java.util.Objects;
 
 @ControllerAdvice
 public class ControllerExceptionHandler {
@@ -35,6 +37,19 @@ public class ControllerExceptionHandler {
 
         return new ResponseEntity<>(message, HttpStatus.BAD_REQUEST);
     }
+
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public ResponseEntity<ErrorMessage> httpMessageNotReadableException(HttpRequestMethodNotSupportedException ex, WebRequest request) {
+
+        ErrorMessage message = new ErrorMessage(
+                HttpStatus.METHOD_NOT_ALLOWED.toString(),
+                ex.getMessage().concat(". Supported methods ").concat(Objects.requireNonNull(ex.getSupportedHttpMethods()).toString()),
+                request.getDescription(false),
+                ZonedDateTime.now());
+
+        return new ResponseEntity<>(message, HttpStatus.METHOD_NOT_ALLOWED);
+    }
+
 
 
 }
